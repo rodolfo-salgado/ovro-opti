0bfc44fc7cb3f3a5f0b11fd3907eeada7725a50a
rodolfo-salgado <70605215+rodolfo-salgado@users.noreply.github.com>
2024-03-15 13:21:00 -0300

    implemented a mutation operator based on permutations

--- sky_optimizer.py
+++ sky_optimizer.py
@@ -11,15 +11,15 @@ class SkyOptimizer:
         match optimizer:
             case 'genetic':
                 self.pop_size = kwargs['pop_size']
-                # self.parent_num = kwargs['parent_num']
                 self.offsp_size = kwargs['offsp_size']
-                self.mut_prob = kwargs['mut_prob']
                 self.gen_num = kwargs['gen_num']
                 self.mut_prob = kwargs['mut_prob']
+                self.mut_rate = kwargs['mut_rate']
                 self.optimizer = self.opt_genetic
                 self.set_pop_generator(kwargs['pop_gen'])
                 self.set_par_selector(kwargs['selector'])
-                self.set_crossover(kwargs['crossover'])
+                self.set_crossover(kwargs['crossover_op'])
+                self.set_mutop(kwargs['mut_op'])
                 self.set_survop(kwargs['surv_op'])
     
     def set_pop_generator(self, generator):
@@ -30,17 +30,22 @@ class SkyOptimizer:
     def set_par_selector(self, selector):
         match selector:
             case 'natural':
-                self.select_par = self.parsel_natural
+                self.selection = self.parsel_natural
     
     def set_crossover(self, crossover_op):
         match crossover_op:
             case 'single_point':
                 self.crossover = self.crossover_single
+    
+    def set_mutop(self, mut_op):
+        match mut_op:
+            case 'permutate':
+                self.mutation = self.mutop_permutate
 
     def set_survop(self, surv_op):
         match surv_op:
             case 'replace':
-                self.survop = self.survop_replace
+                self.survival = self.survop_replace
 
     def run_optimizer(self, print_output=True):
         self.optimizer()
@@ -79,11 +84,23 @@ class SkyOptimizer:
     def survop_replace(self, pop, children):
         return children
 
+    # Mutation operators
+    def mutop_permutate(self, children):
+        for c in children:
+            if self.mut_prob > random.random():
+                n = len(c)
+                mut_num = int(self.mut_rate * n / 2)
+                for _ in range(mut_num):
+                    i = random.randint(0, n - 1)
+                    j = random.randint(0, n - 1)
+                    c[i], c[j] = c[j], c[i]
+
     def opt_genetic(self):
         pop = self.new_pop()
         pbar = trange(self.gen_num)
         for gen in pbar:
-            parents = self.select_par(pop, self.offsp_size)
+            parents = self.selection(pop, self.offsp_size)
             children = self.crossover(parents)
-            pop = self.survop(pop, children)
+            children = self.mutation(children)
+            pop = self.survival(pop, children)
         return pop
