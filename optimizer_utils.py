11b3e734abfc32727ca4a24f6c6e14a906059e99
rodolfo-salgado <70605215+rodolfo-salgado@users.noreply.github.com>
2024-03-21 11:09:46 -0300

    added get_time_detail()

--- optimizer_utils.py
+++ optimizer_utils.py
@@ -951,3 +951,32 @@ def place_calibrator(regions_order, lst_start, regions, sources, calibrators, \
     new_order.insert(last_i, best_cal)
     print(f'Insert cal {best_cal} at i={last_i}')
     return new_order
+
+def get_time_detail(regions, regions_order, lst_start, sources, \
+    za_t=0, az_t=180):
+    """Returns total time for an observation cycle (wait + slew + obs).
+    """
+    lst = lst_start % 24
+    Time = []
+    for R in regions_order:
+        # Get region
+        curr_region = regions[R]
+        # Add wait time
+        t_wait = cu.get_wait_time(curr_region['obs_range'], lst)
+        lst += t_wait
+        # Get region coords
+        za_c, az_c = cu.radec_zaaz(curr_region['ra'], curr_region['dec'], lst)
+        az_c = tel_data.move_in_azimuth(az_t, az_c)
+        # Get slew_time
+        t_slew = tel_data.slew_time(za_t, az_t, za_c, az_c)
+        lst += t_slew
+        # Get observation time
+        t_obs = curr_region['obstime']
+        lst += t_obs
+        # Move to last source in region
+        za_ls, az_ls = position_last_source_on_region(curr_region, sources, lst)
+        az_ls = tel_data.move_in_azimuth(az_t, az_ls)
+        za_t, az_t = za_ls, az_ls
+        # Add times
+        Time.append((R, t_wait, t_slew, t_obs, lst))
+    return Time
