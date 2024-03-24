6625b092080138524446322e774c16f1cc4c97b9
rodolfo-salgado <70605215+rodolfo-salgado@users.noreply.github.com>
2024-03-24 20:19:39 -0300

    added legend to plot

--- plot_utils.py
+++ plot_utils.py
@@ -1,5 +1,6 @@
 import optimizer_utils as opt
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 
 def plot_order(order, reg_dict, src_dict, cal_dict, path=None, \
     reg_labels=False, mark_cals=True):
@@ -28,6 +29,11 @@ def plot_order(order, reg_dict, src_dict, cal_dict, path=None, \
     ax.set_xlabel('Regions')
     ax.set_title(f'Total time: {t_wait + t_slew + t_obs:.2f} h\n' + \
         f'Wait time: {t_wait:.2f} h, Slew time: {t_slew:.2f} h, Obs. time: {t_obs:.2f} h')
+    legends = [ Line2D([], [], color='k', lw=5, label='Wait time'),
+                Line2D([], [], color='y', lw=5, label='Slew time'),
+                Line2D([], [], color='c', lw=5, label='Obs. time'),
+                Line2D([], [], linestyle='None', color='r', marker='v', label='Calibrator')]
+    ax.legend(handles=legends, loc='upper left')
     if reg_labels:
         ax.set_xticks(list(range(len(Time))), labels=reg_label)
         ax.tick_params(axis='x', labelsize=6, rotation=45, labelrotation=90)
